09b48a5a72ea01f12dcbe6fbe23c90d2d56b8e4b
GhislainKoffi <leguedekoffighislain@gmail.com>
2026-04-20 10:18:32 +0000

    Revert "Ajout du dossier engine et du fichier main.py"
    This reverts commit 590a62be15d1cfc6efac3d42bd9e167af56942e1.

--- main.py
+++
@@ -1,8 +0,0 @@
-from engine.core.engine import run_scan
-import json
-
-if __name__ == "__main__":
-    target = input("Enter target URL: ")
-    result = run_scan(target)
-
-    print(json.dumps(result, indent=2))
